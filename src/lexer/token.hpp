4582d32c66179f60d96348d94a8963f535e5b006
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-07-31 09:17:36 +0200

    FORMAT: reformatted token.hpp

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -1,148 +1,152 @@
 #pragma once
 
-// 
+//
 // TOKEN.hpp
 //
 // layouts the Token class
 //
 
 #include "../snippets.h"
+
 #include <initializer_list>
 #include <vector>
 
 namespace lexer {
     class Token : public Repr {
-    /**
-     * @class Holds functions adding to the tokenizing features
-    */
+            /**
+             * @class Holds functions adding to the tokenizing features
+             */
         protected:
-        virtual String _str() const;
+            virtual String _str() const;
 
         public:
-        enum Type {
-            /**
-             * @enum of all available Token types
-            */
+            enum Type {
+                /**
+                 * @enum of all available Token types
+                 */
 
-                //  SPECIAL   //
-            NONE     ,        // undefined token
-            ID       ,        // a name
-            EF       ,        // File end
-            END_CMD  ,        // ;
+                // clang-format off
+            
+                    //  SPECIAL   //
+                NONE     ,        // undefined token
+                ID       ,        // a name
+                EF       ,        // File end
+                END_CMD  ,        // ;
 
-                //  LITERALS  //
-            INT      ,        // int literal
-            HEX      ,        // int literal in hexadecimal
-            BINARY   ,        // int literal in binary
-            BOOL     ,        // bool literal
-            STRING   ,        // String literal
-            CHAR     ,        // char literal
-            FLOAT    ,        // float literal
-            NULV     ,        // "null"
+                    //  LITERALS  //
+                INT      ,        // int literal
+                HEX      ,        // int literal in hexadecimal
+                BINARY   ,        // int literal in binary
+                BOOL     ,        // bool literal
+                STRING   ,        // String literal
+                CHAR     ,        // char literal
+                FLOAT    ,        // float literal
+                NULV     ,        // "null"
 
-                //    MATH    //
-            SET      ,        // =
-            ADD      ,        // +
-            SUB      ,        // -
-            MUL      ,        // *
-            DIV      ,        // /
-            MOD      ,        // %
-            POW      ,        // **
-            INC      ,        // ++
-            DEC      ,        // --
-            NEC      ,        // - <int>
+                    //    MATH    //
+                SET      ,        // =
+                ADD      ,        // +
+                SUB      ,        // -
+                MUL      ,        // *
+                DIV      ,        // /
+                MOD      ,        // %
+                POW      ,        // **
+                INC      ,        // ++
+                DEC      ,        // --
+                NEC      ,        // - <int>
 
-            NEG      ,        // ~
-            AND      ,        // &
-            OR       ,        // |
-            XOR      ,        // ^
-            SHL      ,        // <<
-            SHR      ,        // >>
-            LSHR     ,        // !>
+                NEG      ,        // ~
+                AND      ,        // &
+                OR       ,        // |
+                XOR      ,        // ^
+                SHL      ,        // <<
+                SHR      ,        // >>
+                LSHR     ,        // !>
 
-                //  LOGICAL   //
-            LAND     ,        // &&
-            LOR      ,        // ||
-            NOT      ,        // !
+                    //  LOGICAL   //
+                LAND     ,        // &&
+                LOR      ,        // ||
+                NOT      ,        // !
 
-                // COMPARISON //
-            EQ       ,        // ==
-            NEQ      ,        // !=
-            LESS     ,        // <
-            GREATER  ,        // >
-            GEQ      ,        // >=
-            LEQ      ,        // <=
+                    // COMPARISON //
+                EQ       ,        // ==
+                NEQ      ,        // !=
+                LESS     ,        // <
+                GREATER  ,        // >
+                GEQ      ,        // >=
+                LEQ      ,        // <=
 
-                //    FLOW    //
-            QM       ,        // ?
-            IN       ,        // :
-            UNPACK   ,        // <-
-            ADDR     ,        // #
-            LIFETIME ,        // @
-            SUBNS    ,        // ::
-            ACCESS   ,        // .
-            COMMA    ,        // ,
-            DOTDOT   ,        // ..
-            DOTDOTDOT,        // ...
+                    //    FLOW    //
+                QM       ,        // ?
+                IN       ,        // :
+                UNPACK   ,        // <-
+                ADDR     ,        // #
+                LIFETIME ,        // @
+                SUBNS    ,        // ::
+                ACCESS   ,        // .
+                COMMA    ,        // ,
+                DOTDOT   ,        // ..
+                DOTDOTDOT,        // ...
 
-                // PARANTHESIS //
-            OPEN     ,        // (
-            CLOSE    ,        // )
-            BLOCK_OPEN ,      // {
-            BLOCK_CLOSE,      // }
-            INDEX_OPEN ,      // [
-            INDEX_CLOSE,      // ]
+                    // PARANTHESIS //
+                OPEN     ,        // (
+                CLOSE    ,        // )
+                BLOCK_OPEN ,      // {
+                BLOCK_CLOSE,      // }
+                INDEX_OPEN ,      // [
+                INDEX_CLOSE,      // ]
 
-                //  KEYWORDS  //
-            IF        ,         
-            ELSE      ,         
-            FOR       ,   
-            WHILE     ,        
-            DO        ,    
-            THROW     ,      
-            CATCH     ,
-            TRY       ,
-            BREAK     ,
-            CONTINUE  ,
-            NOIMPL    ,
-            RETURN    ,
-            AS        ,
-            OPERATOR  ,
-            SWITCH    ,
-            CASE      ,
+                    //  KEYWORDS  //
+                IF        ,         
+                ELSE      ,         
+                FOR       ,   
+                WHILE     ,        
+                DO        ,    
+                THROW     ,      
+                CATCH     ,
+                TRY       ,
+                BREAK     ,
+                CONTINUE  ,
+                NOIMPL    ,
+                RETURN    ,
+                AS        ,
+                OPERATOR  ,
+                SWITCH    ,
+                CASE      ,
 
-            IMPORT    ,
-            CLASS     ,
-            ENUM      ,
-            STRUCT    ,
-            VIRTUAL   ,
-            ABSTRACT  ,
-            FRIEND    ,
-            PROTECTED ,
-            PRIVATE   ,
-            STATIC    ,
-            PUBLIC    ,
-            MUT       ,
-            CONST     ,
-            NAMESPACE ,
-            NEW       ,
-            FINALLY   ,
-            DELETE    ,
-            NOWRAP   
-        };
+                IMPORT    ,
+                CLASS     ,
+                ENUM      ,
+                STRUCT    ,
+                VIRTUAL   ,
+                ABSTRACT  ,
+                FRIEND    ,
+                PROTECTED ,
+                PRIVATE   ,
+                STATIC    ,
+                PUBLIC    ,
+                MUT       ,
+                CONST     ,
+                NAMESPACE ,
+                NEW       ,
+                FINALLY   ,
+                DELETE    ,
+                NOWRAP
+                // clang-format on
+            };
 
-        Type type;     //> this tokens type
-        String value;  //> this tokens contents
+            Type   type;  //> this tokens type
+            String value; //> this tokens contents
 
-        uint64 l, c;                //> this tokens position in the File
-        String filename;            //> this tokens File's name (for error messages)
-        sptr<String> line_contents; //> this tokens line's contents (for error messages)
+            uint64       l, c;          //> this tokens position in the File
+            String       filename;      //> this tokens File's name (for error messages)
+            sptr<String> line_contents; //> this tokens line's contents (for error messages)
 
-        Token(Type t, String content, uint64 l, uint64 c, String filename, sptr<String> lc);
-        Token() = default;
-        virtual ~Token();
+            Token(Type t, String content, uint64 l, uint64 c, String filename, sptr<String> lc);
+            Token() = default;
+            virtual ~Token();
 
-        bool operator == (Token other);
+            bool operator==(Token other);
     };
 
     /**
@@ -151,72 +155,72 @@ namespace lexer {
     String getTokenName(Token::Type);
 
     class TokenStream final : public Repr {
-        
         public:
-        std::vector<Token> tokens;
+            std::vector<Token> tokens;
 
         protected:
-        String _str() const {
-            String s;
-            for (Token t : tokens) {
-                s += t.value + " ";
+            String _str() const {
+                String s;
+                for (Token t : tokens) { s += t.value + " "; }
+                return s;
             }
-            return s;
-        }
 
         public:
-        class Match final {
-            uint64 at = 0;
-            bool   was_found = false;
-            const TokenStream* on = nullptr;
+            class Match final {
+                    uint64             at        = 0;
+                    bool               was_found = false;
+                    const TokenStream* on        = nullptr;
 
-            public:
-            Match(uint64 at, bool found, const TokenStream* on) {
-                this->at = at;
-                this->was_found = found;
-                this->on = on;
+                public:
+                    Match(uint64 at, bool found, const TokenStream* on) {
+                        this->at        = at;
+                        this->was_found = found;
+                        this->on        = on;
+                    }
+
+                    ~Match() = default;
+
+                    inline bool found() const { return was_found; }
+
+                    TokenStream before() const { return found() ? on->slice(0, 1, at) : TokenStream({}); }
+
+                    TokenStream after() const { return found() ? on->slice(at + 1, 1, on->size()) : TokenStream({}); }
+
+                    operator uint64() { return at; }
+
+                    operator int64() { return at; }
+
+                    operator bool() { return was_found; }
+            };
+
+            TokenStream(std::vector<Token> tokens) { this->tokens = tokens; }
+
+            TokenStream slice(int64 start, int64 step, int64 stop) const;
+
+            TokenStream getTS(int64 idx) {
+                if (idx < 0) { idx += size(); }
+                return TokenStream({tokens.at(idx)});
             }
-            ~Match() = default;
 
-            inline bool found() const { return was_found; }
-            TokenStream before() const { return found() ? on->slice(0, 1, at) : TokenStream({}); }
-            TokenStream after() const { return found() ? on->slice(at + 1, 1, on->size()) : TokenStream({}); }
-
-            operator uint64() { return at; }
-            operator int64() { return at; }
-            operator bool() {
-                return was_found;
+            Token operator[](int64 idx) const {
+                if (idx < 0) { idx += size(); }
+                return tokens.at(idx);
             }
-        };
-        
-        TokenStream(std::vector<Token> tokens){this->tokens = tokens;}
 
-        TokenStream slice(int64 start, int64 step, int64 stop) const;
+            inline uint64 size() const noexcept { return tokens.size(); }
 
-        TokenStream getTS(int64 idx) {
-            if (idx < 0)
-                idx += size();
-            return TokenStream({tokens.at(idx)});
-        }
+            Match splitStack(std::initializer_list<lexer::Token::Type>, uint64 start_idx = 0) const;
+            Match rsplitStack(std::initializer_list<lexer::Token::Type>, uint64 start_idx = 0) const;
 
-        Token operator[](int64 idx) const {
-            if (idx < 0)
-                idx += size();
-            return tokens.at(idx);
-        }
-        inline uint64 size() const noexcept { return tokens.size(); }
+            Match split(std::initializer_list<lexer::Token::Type>, uint64 start_idx = 0) const;
+            Match rsplit(std::initializer_list<lexer::Token::Type>, uint64 start_idx = 0) const;
 
-        Match splitStack(std::initializer_list<lexer::Token::Type>, uint64 start_idx=0) const;
-        Match rsplitStack(std::initializer_list<lexer::Token::Type>, uint64 start_idx=0) const;
+            inline Match operator[](lexer::Token::Type type) { return split({type}); }
 
-        Match split(std::initializer_list<lexer::Token::Type>, uint64 start_idx=0) const;
-        Match rsplit(std::initializer_list<lexer::Token::Type>, uint64 start_idx=0) const;
-
-        inline Match operator [] (lexer::Token::Type type){return split({type});}
-
-        inline bool empty() const { return size() == 0; }
+            inline bool empty() const { return size() == 0; }
     };
-}
+} // namespace lexer
 
 const lexer::Token nullToken = lexer::Token(lexer::Token::NONE, "", 0, 0, "", nullptr);
+
 
