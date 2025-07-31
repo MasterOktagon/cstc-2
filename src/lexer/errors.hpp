f133c6e285b7fc1201f0fdbb5b23bcffd6b514aa
MasterOktagon <userkontakt.hexagongames@gmail.com>
2025-07-31 09:15:10 +0200

    FORMAT: reformatted errors.hpp

--- src/lexer/errors.hpp
+++ src/lexer/errors.hpp
@@ -8,6 +8,7 @@
 
 #include "../snippets.h"
 #include "token.hpp"
+
 #include <vector>
 
 /*
@@ -16,8 +17,8 @@ but this might change in the future, so the lexer errors are different functions
 */
 
 namespace lexer {
-    extern void error(String name, std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix="");
-    extern void warn (String name, std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix="");
-    extern void note (std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix="");
-}
+    extern void error(String name, std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix = "");
+    extern void warn(String name, std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix = "");
+    extern void note(std::vector<lexer::Token> tokens, String msg, uint32 code, String appendix = "");
+} // namespace lexer
 
